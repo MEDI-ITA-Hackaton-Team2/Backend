d2aab6e8f633ebe317f5887c26a2cded9426b76f
juuns <juuns@ajou.ac.kr>
2024-05-04 06:44:48 +0900

    fix: 필드 추가

--- src/main/java/com/hackathon/server/dto/IngredientRes.java
+++ src/main/java/com/hackathon/server/dto/IngredientRes.java
@@ -9,11 +9,13 @@ public class IngredientRes {
     private final String ingredientName;
     private final String quantity;
     private final Double price;
+    private final String ingredientImageURL;
 
     public IngredientRes(RawIngredient rawIngredient) {
         this.ingredientId = rawIngredient.getIngredientId();
         this.ingredientName = rawIngredient.getIngredientName();
         this.quantity = rawIngredient.getQuantity();
         this.price = rawIngredient.getPrice();
+        this.ingredientImageURL = rawIngredient.getIngredientImageURL();
     }
 }
